2f3a699a32b353279d09267dcf8c0d8fb767c264
IanGichengo <iangatimu6@gmail.com>
2024-04-19 06:20:17 -0400

    puppet_lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # install flask from pip3 using puppet
-package { 'python3-pip':
-  ensure => present,
-}
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
