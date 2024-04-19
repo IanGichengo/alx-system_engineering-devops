55e0e3a007bd991abd223e7f8e01c20a057a556d
IanGichengo <iangatimu6@gmail.com>
2024-04-19 06:41:25 -0400

    puppet_lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,14 @@
 # install flask from pip3 using puppet
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
 package { 'python3-pip':
   ensure => installed,
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   path    => ['/bin', '/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0" && /usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
 }
