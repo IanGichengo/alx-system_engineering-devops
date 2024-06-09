86da5c048281236023cbbc3657f54fcf31997d8b
IanGichengo <iangatimu6@gmail.com>
2024-06-09 10:17:23 -0400

    web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fix the internal error on the server
   $php_rout = '/var/www/html/wp-settings.php'
-
   exec { 'replace_line':
     command => "sed -i 's/phpp/php/g' ${php_rout}",
     path    => ['/bin','/usr/bin']
