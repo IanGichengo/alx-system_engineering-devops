3cc8b3c10173f90bedd07cd5d6fe29c9b269ea68
IanGichengo <iangatimu6@gmail.com>
2024-06-17 06:10:47 -0400

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My Reddit Subscribers Checker"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My Agent'}
 
-    try:
-        response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
-    except (requests.RequestException, KeyError):
+        subs = data.get('data').get('subscribers')
+        return subs
+    else:
         return 0
